17ce69f5a4cb97c76123fc3c4f47074fe98313f0
Felix <felix1.mueller@stud.uni-regensburg.de>
2014-07-10 12:00:38 +0200

    added farbentwuerfe

--- src/laravel/app/database/seeds/BroadcastsTableSeeder.php
+++ src/laravel/app/database/seeds/BroadcastsTableSeeder.php
@@ -11,36 +11,6 @@ class BroadcastsTableSeeder extends Seeder {
 	{
 		\DB::table('broadcasts')->truncate();
         
-		\DB::table('broadcasts')->insert(array (
-			0 => 
-			array (
-				'id' => 1,
-				'title' => 'Tatort',
-				'datetime' => '0000-00-00 00:00:00',
-				'url' => 'http://www.daserste.de',
-				'duration' => '00:00:00',
-				'image' => '',
-				'created_at' => '2014-07-08 12:56:04',
-				'updated_at' => '2014-07-08 12:56:04',
-				'station_id' => 1,
-				'playlist_id' => NULL,
-				'user_id' => 1,
-			),
-			1 => 
-			array (
-				'id' => 2,
-				'title' => 'Heute Journal',
-				'datetime' => '0000-00-00 00:00:00',
-				'url' => 'http://www.zdf.de',
-				'duration' => '00:00:00',
-				'image' => '',
-				'created_at' => '2014-07-08 12:56:04',
-				'updated_at' => '2014-07-08 12:56:04',
-				'station_id' => 2,
-				'playlist_id' => NULL,
-				'user_id' => 1,
-			),
-		));
 	}
 
 }
--- src/laravel/app/database/seeds/UsersTableSeeder.php
+++ src/laravel/app/database/seeds/UsersTableSeeder.php
@@ -11,20 +11,6 @@ class UsersTableSeeder extends Seeder {
 	{
 		\DB::table('users')->truncate();
         
-		\DB::table('users')->insert(array (
-			0 => 
-			array (
-				'id' => 1,
-				'email' => 'test',
-				'password' => '$2y$10$1F6zZz8fg6gwXTgHP8i4cePHkCncfhfTCd.NmYO3n8nePsn8nf6fa',
-				'password_temp' => '',
-				'code' => '',
-				'active' => 1,
-				'remember_token' => '',
-				'created_at' => '2014-07-08 12:56:04',
-				'updated_at' => '2014-07-08 12:56:04',
-			),
-		));
 	}
 
 }
