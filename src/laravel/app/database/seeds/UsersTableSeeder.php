71e112b6db2080174222c2996316d63ba56de7ed
Felix <felix1.mueller@stud.uni-regensburg.de>
2014-07-15 12:46:56 +0200

    updated nav

--- src/laravel/app/database/seeds/UsersTableSeeder.php
+++ src/laravel/app/database/seeds/UsersTableSeeder.php
@@ -11,20 +11,6 @@ class UsersTableSeeder extends Seeder {
 	{
 		\DB::table('users')->truncate();
         
-		\DB::table('users')->insert(array (
-			0 => 
-			array (
-				'id' => 1,
-				'email' => 'test',
-				'password' => '$2y$10$tzhcc5Cp/iHEkzJs6OdbZutBiodpfzLp5AXaHwiDO55ck8MKe78Xe',
-				'password_temp' => '',
-				'code' => '',
-				'active' => 1,
-				'remember_token' => '',
-				'created_at' => '0000-00-00 00:00:00',
-				'updated_at' => '0000-00-00 00:00:00',
-			),
-		));
 	}
 
 }
