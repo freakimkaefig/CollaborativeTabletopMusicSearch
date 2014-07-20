a8267c945a199bd8fcca9fe062651fecc465ecfb
Felix <felix1.mueller@stud.uni-regensburg.de>
2014-07-20 13:04:09 +0200

    video-page videos responsive

--- src/laravel/app/views/streaming/video.blade.php
+++ src/laravel/app/views/streaming/video.blade.php
@@ -5,7 +5,7 @@
 	@if(isset($video))
 		<div class="row">
 			<div id="video-wrapper" class=" col-xs-12 col-sm-8 colsm-offset-0 col-lg-7 col-lg-offset-1">
-				<video id="video" class="video-js vjs-default-skin" controls preload="auto"></video>
+				<video id="video" class="video-js vjs-default-skin img-responsive" controls preload="auto"></video>
 			</div>
 			<div id="info-wrapper"></div>
 		</div>
