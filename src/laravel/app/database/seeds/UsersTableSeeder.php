1d2578bcc1a3aafe9cfc74ea41516492310452d9
Felix <felix1.mueller@stud.uni-regensburg.de>
2014-08-10 19:52:27 +0200

    database problems

--- src/laravel/app/database/seeds/UsersTableSeeder.php
+++ src/laravel/app/database/seeds/UsersTableSeeder.php
@@ -11,20 +11,6 @@ class UsersTableSeeder extends Seeder {
 	{
 		\DB::table('users')->truncate();
         
-		\DB::table('users')->insert(array (
-			0 => 
-			array (
-				'id' => 1,
-				'email' => 'mueller-felix@web.de',
-				'password' => '$2y$10$fb6K/1kYeJ6kEl2huWbCFu1EV.v87NZS4quph4ggzXpCtBNUM6xAa',
-				'password_temp' => '',
-				'code' => '',
-				'active' => 1,
-				'remember_token' => '',
-				'created_at' => '2014-08-10 15:05:43',
-				'updated_at' => '2014-08-10 15:06:23',
-			),
-		));
 	}
 
 }
