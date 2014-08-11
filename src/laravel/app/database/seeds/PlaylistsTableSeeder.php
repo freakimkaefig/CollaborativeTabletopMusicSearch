06f9b068a2623bb4fe1fb604267404faec772b15
Felix <felix1.mueller@stud.uni-regensburg.de>
2014-08-11 11:02:39 +0200

    changed database, added broadcasts->subtitle

--- src/laravel/app/database/seeds/PlaylistsTableSeeder.php
+++ src/laravel/app/database/seeds/PlaylistsTableSeeder.php
@@ -15,19 +15,12 @@ class PlaylistsTableSeeder extends Seeder {
 			0 => 
 			array (
 				'id' => 1,
-				'name' => '',
-				'created_at' => '2014-07-29 12:05:09',
-				'updated_at' => '2014-07-29 12:05:09',
-				'user' => 2,
-			),
-			1 => 
-			array (
-				'id' => 2,
 				'name' => 'TestPlaylist',
-				'created_at' => '2014-07-30 13:21:52',
-				'updated_at' => '2014-07-30 13:21:52',
+				'created_at' => '2014-07-23 21:06:13',
+				'updated_at' => '2014-07-23 21:06:13',
 				'user' => 1,
 			),
 		));
 	}
+
 }
