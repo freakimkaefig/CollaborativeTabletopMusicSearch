a947e5ea945b1cf4256c81313a00f6c8b64295d3
semmler1988 <semmel-tobi@gmx.de>
2014-08-12 16:06:16 +0200

    added search mobile

---
+++ src/laravel/app/views/search/search_mobile.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Mediathek Crawler</title>
+		{{ HTML::style('css/bootstrap.min.css'); }}
+		{{ HTML::style('css/video-js.css') }}
+		{{ HTML::style('css/style.css') }}
+
+		{{ HTML::script('js/libs/jquery/jquery.min.js') }}
+		{{ HTML::script('js/libs/bootstrap/bootstrap.min.js') }}
+		
+	</head>
+	<body>			
+		<div class="form-group">
+			<input type="text" class="form-control" name="search" placeholder="Suche"{{ (Input::old('search')) ? ' value="' . e(Input::old('search')) . '"' : '' }}>
+		</div>
+
+
+
+		<div id="category-filter" class="form-group col-sm-3">
+		<h4>Rubriken</h4>
+		<div class="col-sm-6">
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" name="nachrichten" value="nachrichten"{{ (Input::old('nachrichten')) ? ' checked' : ' selected' }}>
+					Nachrichten
+				</label>
+			</div>
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" name="sport" value="sport" >
+					Sport
+				</label>
+			</div>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="politik" value="politik" >
+					Politik
+				</label>
+			</div>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="wirtschaft" value="wirtschaft" >
+					Wirtschaft
+				</label>
+			</div>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="kinder" value="kinder" >
+					Kinder
+				</label>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="kino-tv" value="kino-tv" >
+					Kino und TV
+				</label>
+			</div>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="wissen-kultur" value="wissen-kultur" >
+					Wissen
+				</label>
+			</div>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="ratgeber-gesundheit" value="ratgeber-gesundheit" 
+					>
+					Ratgeber
+				</label>
+			</div>
+			<div class="checkbox ">
+				<label>
+					<input type="checkbox" name="unterhaltung" value="unterhaltung" >
+					Unterhaltung
+				</label>
+			</div>
+		</div>
+	</div>
+	<div id="channel-filter" class="form-group col-sm-3">
+		<h4>Sender</h4>
+		<div class="col-sm-6">
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" name="daserste" value="daserste">
+					Das Erste
+				</label>
+			</div>
+			<div class="checkbox disabled">
+				<label>
+					<input type="checkbox" name="zdf" value="zdf">
+					ZDF
+				</label>
+			</div>
+			<div class="checkbox disabled">
+				<label>
+					<input type="checkbox" name="arte" value="arte">
+					Arte
+				</label>
+			</div>
+		</div>		
+	</div>
+	<div class="form-group col-sm-4 form-inline">
+		<h4>Datum &amp; Dauer</h4>
+		<div class="disabled col-sm-6">
+			<label>Von:
+				<input type="date" name="von" value="von" disabled>
+			</label>
+		</div>
+		<div class="disabled col-sm-6">
+			<label>Bis:
+				<input type="date" name="bis" value="bis" disabled>
+			</label>
+		</div>
+		<div class="disabled col-sm-6">
+			<label>Dauer in min
+				<input type="range" name="dauer" value="dauer" min="1" max="90" disabled>
+			</label>
+		</div>
+	</div>
+
+	<div class="col-sm-2">
+		<button id="submit" class="btn btn-transparent col-xs-12">Absenden</button>
+		<button type="" class="btn btn-transparent col-xs-12">Zurücksetzen</button>
+	</div>
+		@include('layout.header')
+
+		@include('mediathek-crawler-js')
+
+	</body>
+</html>
