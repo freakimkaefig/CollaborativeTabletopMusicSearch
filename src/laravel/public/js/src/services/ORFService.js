9a0e9368c82d09a642eda07207f1092ea44956bf
led13714 <david.lechler@stud.uni-regensburg.de>
2014-08-22 16:37:11 +0200

    implemented ORFService

--- src/laravel/public/js/src/services/ORFService.js
+++ src/laravel/public/js/src/services/ORFService.js
@@ -9,7 +9,8 @@ MediathekCrawler.ORFService = function() {
 
 	// constants for searching
 	SEARCH_URL = 'http://tvthek.orf.at/search?q=',
-	// SEARCH_URL = 'http://www1.ORF.de/themen/suche/suche100.jsp?q=klofrau',
+	SEARCH_NEW = 'http://tvthek.orf.at/newest',
+	SEARCH_HOT = 'http://tvthek.orf.at/most_viewed',
 	SEARCH_PARAMS = '', 
 	SEARCH_PAGE2 = ''
 	STREAM_URL = '',
@@ -58,81 +59,104 @@ MediathekCrawler.ORFService = function() {
 
 		var _searchUrl = PROXY_URL + encodeURI(SEARCH_URL + searchStr/* + SEARCH_PARAMS*/);
 		// var _searchUrl = PROXY_URL + "www.ORF.de/mediathek/video/suche/videosucheinclude100-solrSendereihenSuche_index-mediathekvideos.jsp";
-		console.log('ORF searchString url: ',_searchUrl);
+		// console.log('ORF searchString url: ',_searchUrl);
 		$.ajax({
 			url: _searchUrl,
 			type: 'GET',
 			success: function(data){
 				// console.log('ORF data: ',data);
-				onORFSearchString(data, origin);
+				onORFLoadDetails(data, origin);
 			}
 		});
 	},
 
-	onORFSearchString = function(data, origin) {
+	getNew = function(maxResults){
+		var origin = {
+			_channel: 'ORF',
+			_method: 'getNew',
+			_searchTerm: null,
+			_badge: 'new'
+		};
+
+		var _searchUrl = PROXY_URL + encodeURI(SEARCH_NEW);
+		// console.log('ORF getNew url: ',_searchUrl);
+		$.ajax({
+			url: _searchUrl,
+			type: 'GET',
+			success: function(data){
+				// console.log('ORF data: ',data);
+				onORFLoadDetails(data, origin, maxResults);
+			}
+		});
+	},
+
+	getHot = function(){
+		var origin = {
+			_channel: 'ORF',
+			_method: 'getHot',
+			_searchTerm: null,
+			_badge: 'hot'
+		};
+
+		var _searchUrl = PROXY_URL + encodeURI(SEARCH_HOT);
+		// console.log('ORF getNew url: ',_searchUrl);
+		$.ajax({
+			url: _searchUrl,
+			type: 'GET',
+			success: function(data){
+				// console.log('ORF data: ',data);
+				onORFLoadDetails(data, origin);
+			}
+		});
+	},
+
+	onORFLoadDetails = function(data, origin, maxResults) {
 
 		// console.log('ORF pageHeadline: ',pageHeadline);
 		// find all divs with the class 'teaser'
+		if(!maxResults || maxResults === undefined || maxResults === null){
+			maxResults = 15;
+		}
+		var counter = 1;
 		var x = $(data).find('.item');
 		x.each(function (index, element) {
-			// check if element has child with class 'media' and 'mediaA'
-			if($(element).length > 0) {
-				// get 'documentId' of video
-				var	documentUrl = $(element).find('a').attr('href'),
-					// broadcastUrlParams = _parseQueryString($(element).find(VIDEO_CLASS).find(LINK_CLASS).attr('href')),
-					// documentId = broadcastUrlParams.documentId;
-			// console.log('ORF onORFSearchString: ',documentUrl,documentId);
-				
-				// find container for details
-				// var $textWrapper = $(element).find('div.textWrapper');
-				_result = {};
-				_result._station ='ORF';
-			// console.log('ORF onORFSearchString STATION : ',$element.find('p.subtitle').text(), _result._station);
-				_result._title = $(element).find('.item_title').text();
-				_result._subtitle = ''; //$textWrapper.find('p.teasertext').text();
-				_result._length = $(element).find('.meta_duration').text();
-				_result._length = _fixORFLength(_result._length);
-				_result._airtime = $(element).find('.meta_date').text();
-				_result._airtime = _result._airtime.slice(4,100);
-				_result._airtime += ' '+$(element).find('.meta_time').text();
-				_result._airtime = _result._airtime.replace(' Uhr','');
-				_result._teaserImages = [],
-				_result._teaserImages.push(_model.createTeaserImage('395x209', $(element).find('.item_image').find('img').attr('src')));
-				_result._streams = [];
+			if(counter <= maxResults){
 
-				// console.log(documentUrl, _result);
-				loadORFStreams(documentUrl, _result, origin);
+				// check if element has child with class 'media' and 'mediaA'
+				if($(element).length > 0) {
+					// get 'documentId' of video
+					var	documentUrl = $(element).find('a').attr('href'),
+						// broadcastUrlParams = _parseQueryString($(element).find(VIDEO_CLASS).find(LINK_CLASS).attr('href')),
+						// documentId = broadcastUrlParams.documentId;
+				// console.log('ORF onORFLoadDetails: ',documentUrl,documentId);
+					
+					// find container for details
+					// var $textWrapper = $(element).find('div.textWrapper');
+					_result = {};
+					_result._station ='ORF';
+				// console.log('ORF onORFLoadDetails STATION : ',$element.find('p.subtitle').text(), _result._station);
+					_result._title = $(element).find('.item_title').text();
+					_result._subtitle = ''; //$textWrapper.find('p.teasertext').text();
+					_result._length = $(element).find('.meta_duration').text();
+					_result._length = _fixORFLength(_result._length);
+					_result._details = $(element).find('.item_description').text();
+					_result._airtime = $(element).find('.meta_date').text();
+					_result._airtime = _result._airtime.slice(4,100);
+					_result._airtime += ' '+$(element).find('.meta_time').text();
+					_result._airtime = _result._airtime.replace(' Uhr','');
+					_result._teaserImages = [],
+					_result._teaserImages.push(_model.createTeaserImage('395x209', $(element).find('.item_image').find('img').attr('src')));
+					_result._streams = [];
+
+					// console.log(documentUrl, _result);
+					loadORFStreams(documentUrl, _result, origin);
+				}
+				counter++;
 			}
 		});
 
 	},
 
-	// loadDetails = function(result, detailURL, origin) {
-	// 	var detailUrl = PROXY_URL + encodeURI(detailURL);
-	// 			// console.log('ORF loadDetails: ',result,origin, detailUrl,documentId);
-	// 	$.ajax({
-	// 		url: detailUrl,
-	// 		type: 'GET',
-	// 		success: function(data) {
-	// 			onLoadDetails(documentId, result, data,origin);
-	// 		}
-
-	// 	});
-	// },
-
-	// onLoadDetails = function(documentId, result, data,origin) {
-	// 	var _result = result;
-	// 	_result._details = $(data).find(DETAIL_ELEMENT).text();
-
-	// 	// load streams for result
-	// 	loadORFStreams(documentId, _result,origin);
-	// },
-
-	/**
-	 * Private function for async loading of streams for a search result
-	 * @param {String|Integer}		documentId of the broadcast
-	 * @param {object}				currently builded result (missing streams and preview images)
-	 */
 	loadORFStreams = function(documentUrl, result, origin) {
 		var streamURL = PROXY_URL + encodeURI(documentUrl);
 		// console.log('ORF loadORFStreams streamURL: ',streamURL);
@@ -146,47 +170,80 @@ MediathekCrawler.ORFService = function() {
 		});
 	},
 
-	/**
-	 * Callback function for async loading of streams
-	 * @param {String|Integer}		documentId of the broadcast
-	 * @param {object}				currently builded result (missing streams and preview images)
-	 * @param {JSON}				JSON response of stream loading
-	 */
 	onloadORFStreams = function(documentUrl, result, data, origin) {
-		// console.log(documentUrl);
+		// console.log('ORF onloadORFStreams');;
 		try{
 
 			var resp = JSON.parse($(data).find('.service_link_play').attr('data-jsb'));
+			// console.log('ORF: ', resp);
+			var r = resp.video.sources;
+			// console.log('ORF r: ',resp.video.sources);
+			for(i=0;i<=r.length;i++){
+				if(r[i].src.indexOf('.mp4') > 0){
+					var basetype = '',
+			    		quality = '',
+			    		url = r[i].src,
+			    		filesize = 0,
+		    			type = 'video/mp4';
+
+		    			switch (r[i].quality_string) {
+			    			case 'hoch':
+			    				quality = 3;
+			    				break;
+			    			case 'mittel':
+			    				quality = 2;
+			    				break;
+			    			case 'niedrig':
+			    				quality = 1;
+			    				break;
+	    				}
+			    		var stream = _model.createStream(basetype, type, quality, url, filesize);
+			    		//console.log('basetype: ',basetype,', stream: ',stream._url);
+						result._streams.push(stream);
+		// console.log('ORF onloadORFStreams: ', url, quality);
+				}
+			}
+
+			// result._streams
 		}catch(e){
-			var resp = JSON.parse($(data).find('.player_viewport').find('.jsb_VideoPlaylist').attr('data-jsb'));
-			console.log('ORF diffrent style: ', resp);
+			try{
+				var resp = JSON.parse($(data).find('.player_viewport').find('.jsb_VideoPlaylist').attr('data-jsb'));
+				// console.log('ORF resp: ',resp);
+				var r = resp.selected_video.sources;
+				// console.log('ORF r: ',resp.video.sources);
+				for(i=0;i<=r.length;i++){
+					if(r[i].src.indexOf('.mp4') > 0){
+						var basetype = '',
+				    		quality = '',
+				    		url = r[i].src,
+				    		filesize = 0,
+			    			type = 'video/mp4';
+
+		    			switch (r[i].quality_string) {
+			    			case 'hoch':
+			    				quality = 3;
+			    				break;
+			    			case 'mittel':
+			    				quality = 2;
+			    				break;
+			    			case 'niedrig':
+			    				quality = 1;
+			    				break;
+	    				}
+			    		var stream = _model.createStream(basetype, type, quality, url, filesize);
+			    		//console.log('basetype: ',basetype,', stream: ',stream._url);
+						result._streams.push(stream);
+			// console.log('ORF onloadORFStreams: ', url, quality);
+					}
+				}
+			}catch(e){}
 		}
-		// console.log('ORF onloadORFStreams: ',typeof data, data);
-		// console.log('ORF onloadORFStreams: ',typeof resp, resp);
 
 
-		// console.log('ORF onloadORFStreams result: ', result,origin);
 		if(result._streams.length >0){
 			_model.addResults(origin, result._station, result._title, result._subtitle, result._details, result._length, result._airtime, result._teaserImages, result._streams);
 		}
 	},
-
-	// _parseQueryString = function(queryString) {
- //    	var params = {}, queries, temp, i, l;
- 	
- // 		queryString = queryString.split('?')[1];
-
-	//     // Split into key/value pairs
-	//     queries = queryString.split('&');
- 
-	//     // Convert the array of strings into an object
-	//     for (i = 0, l = queries.length; i < l; i++ ) {
-	//         temp = queries[i].split('=');
-	//         params[temp[0]] = temp[1];
-	//     }
-	 
-	//     return params;
-	// },
 
 	dispose = function() {
 		that = {};
@@ -195,6 +252,8 @@ MediathekCrawler.ORFService = function() {
 	that.init = init;
 	that.dispose = dispose;
 	that.searchString = searchString;
+	that.getNew = getNew;
+	that.getHot = getHot;
 
 	return that;
 
