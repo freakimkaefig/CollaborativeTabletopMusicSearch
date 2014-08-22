31dd414ebd9f1e6825ccb8c00d490beb9219f298
Felix <felix1.mueller@stud.uni-regensburg.de>
2014-08-22 14:46:01 +0200

    videosjs on mobile. footer clickable.

--- src/laravel/public/js/src/views/FooterView.js
+++ src/laravel/public/js/src/views/FooterView.js
@@ -2,26 +2,39 @@ MediathekCrawler.FooterView = (function() {
 	var that = {},
 
 	$footerNav = null,
+	closed = true;
 
 	init = function() {
 		console.info('MediathekCrawler.FooterView.init');
 		$footerNav = $('#footer-nav');
 		$footerNav.on('mouseenter', footerOnMouseEnter);
 		$footerNav.on('mouseleave', footerOnMouseLeave);
-		
+		$footerNav.click(function(){
+			console.log("click",closed);
+			if(closed){
+				footerOnMouseEnter();
+				closed = false;
+			}
+			else{
+				footerOnMouseLeave();
+				closed = true;
+			}
+		});
 	},
+
+
 
 	footerOnMouseEnter = function(event) {
 		// console.log(event)
-		$(this).stop(true, false);
-		$(this).animate({
+		$footerNav.stop(true, false);
+		$footerNav.animate({
 			bottom: 0
 		});
 	},
 
 	footerOnMouseLeave = function(event) {
-		$(this).stop(true, false);
-		$(this).animate({
+		$footerNav.stop(true, false);
+		$footerNav.animate({
 			bottom: -130
 		});
 	},
