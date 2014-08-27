2f6186e0c3c0047df428da05899a5cd87a00fb27
freakimkaefig <lukas.lamm89@googlemail.com>
2014-08-27 19:33:07 +0200

    fixed absolute links in footer

--- src/laravel/app/views/layout/footer.blade.php
+++ src/laravel/app/views/layout/footer.blade.php
@@ -6,22 +6,22 @@
 				<h4 class="row"><span class="col-sm-2 glyphicon glyphicon-th pull-left"></span><span class="col-sm-10">Rubriken</span></h4>
 				<div class="col-sm-6 col-md-4 alpha">
 					<ul>
-						<li><a href="http://mediathek-crawler/hot">Hot</a></li>
-						<li><a href="http://mediathek-crawler/new">New</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/politik">Politik</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/sport">Sport</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/kinder">Kinder</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/wissen-kultur">Wissen</a></li>
+						<li><a href="/hot">Hot</a></li>
+						<li><a href="/new">New</a></li>
+						<li><a href="/rubrik/politik">Politik</a></li>
+						<li><a href="/rubrik/sport">Sport</a></li>
+						<li><a href="/rubrik/kinder">Kinder</a></li>
+						<li><a href="/rubrik/wissen-kultur">Wissen</a></li>
 
 					</ul>
 				</div>
 				<div class="col-sm-6 col-md-4 alpha">
 					<ul>
-						<li><a href="http://mediathek-crawler/rubrik/nachrichten">Nachrichten</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/kino-tv">Kino &amp; TV</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/wirtschaft">Wirtschaft</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/ratgeber-gesundheit">Ratgeber</a></li>
-						<li><a href="http://mediathek-crawler/rubrik/unterhaltung">Unterhaltung</a></li>
+						<li><a href="/rubrik/nachrichten">Nachrichten</a></li>
+						<li><a href="/rubrik/kino-tv">Kino &amp; TV</a></li>
+						<li><a href="/rubrik/wirtschaft">Wirtschaft</a></li>
+						<li><a href="/rubrik/ratgeber-gesundheit">Ratgeber</a></li>
+						<li><a href="/rubrik/unterhaltung">Unterhaltung</a></li>
 
 
 					</ul>
@@ -31,12 +31,12 @@
 				<h4 class="row"><span class="col-sm-2 glyphicon glyphicon-film pull-left"></span><span class="col-sm-10">Sender</span></h4>
 				<div class="col-sm-offset-2">
 					<ul>
-					    <li><a href="http://mediathek-crawler/channel/DasErste">Das Erste</a></li>
-					    <li><a href="http://mediathek-crawler/channel/ZDF">ZDF</a></li>
-					    <li><a href="http://mediathek-crawler/channel/ARTE">Arte</a></li>
-					    <li><a href="http://mediathek-crawler/channel/BR">BR</a></li>
-					    <li><a href="http://mediathek-crawler/channel/SRF">SRF</a></li>
-					    <li><a href="http://mediathek-crawler/channel/ORF">ORF</a></li>
+					    <li><a href="/channel/DasErste">Das Erste</a></li>
+					    <li><a href="/channel/ZDF">ZDF</a></li>
+					    <li><a href="/channel/ARTE">Arte</a></li>
+					    <li><a href="/channel/BR">BR</a></li>
+					    <li><a href="/channel/SRF">SRF</a></li>
+					    <li><a href="/channel/ORF">ORF</a></li>
 					</ul>
 				</div>
 			</div>
@@ -44,11 +44,11 @@
 				<h4 class="row"><span class="col-sm-2 glyphicon glyphicon-user pull-left"></span><span class="col-sm-10">Konto</span></h4>
 				<div class="col-sm-offset-1 alpha ">
 					<ul>
-						<li><a href="http://mediathek-crawler/account/my-account">Mein Konto</a></li>
-						<li><a href="http://mediathek-crawler/playlists">Playlisten</a></li>
-						<li><a href="http://mediathek-crawler/bookmarks">Merkliste</a></li>
+						<li><a href="/account/my-account">Mein Konto</a></li>
+						<li><a href="/playlists">Playlisten</a></li>
+						<li><a href="/bookmarks">Merkliste</a></li>
 						
-													<li><a href="http://mediathek-crawler/account/sign-out">Abmelden</a></li>
+													<li><a href="/account/sign-out">Abmelden</a></li>
 					    					</ul>
 				</div>
 			</div>
@@ -56,10 +56,10 @@
 				<h4 class="col-sm-offset-1"><span class="glyphicon glyphicon-chevron-up"></span></h4>
 				<div class="col-sm-4 col-sm-offset-1 alpha">
 					<ul>
-						<li><a href="http://mediathek-crawler">Startseite</a></li>
-					    <li><a href="http://mediathek-crawler/kontakt">Kontakt</a></li>
-			      		<li><a href="http://mediathek-crawler/datenschutz">Datenschutz</a></li>
-			      		<li><a href="http://mediathek-crawler/impressum">Impressum</a></li>
+						<li><a href="">Startseite</a></li>
+					    <li><a href="/kontakt">Kontakt</a></li>
+			      		<li><a href="/datenschutz">Datenschutz</a></li>
+			      		<li><a href="/impressum">Impressum</a></li>
 				    </ul>
 				</div>
 		    </div>
